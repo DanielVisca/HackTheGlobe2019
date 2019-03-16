73bf8541b7318801acbde395db6dc9712e5a947c
Daniel Visca <danielvisca96@gmail.com>
2019-03-16 17:48:39 -0400

    When our twilio number is messaged, there is anow a response

--- TextTrade/tradePlatform/urls.py
+++ TextTrade/tradePlatform/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
 	path('', views.index, name='index'),
-	path('sms_response/', views.sms_response, name='sms_response'),
+	path('sms/', views.sms_response, name='sms_response'),
 	#url(r'^$', views.sms_response, name='sms_response'),
 ]
--- TextTrade/tradePlatform/views.py
+++ TextTrade/tradePlatform/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from twilio.twiml.messaging_response import MessagingResponse
+
+
 # Create your views here.
 def index(request):
 	return HttpResponse("Hello World, you are at the Trade Platform index page.")
 
+@csrf_exempt
 def sms_response(request):
-	return HttpResponse("SMS page.")
+	#Start our TwiML response
+	response = MessagingResponse()
+
+	# Add a text message
+	msg = response.message("Hello and welcome to TextTrade!")
+
+	return HttpResponse(str(response))
 
