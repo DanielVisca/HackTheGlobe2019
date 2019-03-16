00b0ce7ba89e33f321daa1e32929eb3a2b62ff04
Daniel Visca <danielvisca96@gmail.com>
2019-03-16 17:12:56 -0400

    SMS view exists now

--- TextTrade/tradePlatform/urls.py
+++ TextTrade/tradePlatform/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
 	path('', views.index, name='index'),
+	path('sms_response/', views.sms_response, name='sms_response'),
+	#url(r'^$', views.sms_response, name='sms_response'),
 ]
--- TextTrade/tradePlatform/views.py
+++ TextTrade/tradePlatform/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
 	return HttpResponse("Hello World, you are at the Trade Platform index page.")
+
+def sms_response(request):
+	return HttpResponse("SMS page.")
