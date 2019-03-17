17014fd680a8b82546189a9eab40c49816600298
Daniel Visca <danielvisca96@gmail.com>
2019-03-16 20:51:22 -0400

    responds with the twilio number

--- TextTrade/tradePlatform/views.py
+++ TextTrade/tradePlatform/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
 from django.views.decorators.csrf import csrf_exempt
 from twilio.twiml.messaging_response import MessagingResponse
-
+# include decompose in your views.py
+from django_twilio.request import decompose
 
 # Create your views here.
 def index(request):
@@ -10,11 +12,20 @@ def index(request):
 
 @csrf_exempt
 def sms_response(request):
+	"""
+	This is a webhook that recieves text messages.
+	ToDo: new phone numbers are added to database
+
+	"""
+	# from_number = request.form['From']
+	# body = request.form['To']
 	#Start our TwiML response
 	response = MessagingResponse()
 
+	twilio_request = decompose(request)
+	phone_number = twilio_request.to
 	# Add a text message
-	msg = response.message("Hello and welcome to TextTrade!")
+	msg = response.message(str(phone_number))
 
 	return HttpResponse(str(response))
 
