94588d40f2424e93908bffd397e2b9c50890b0e5
avkashm <avkash.mukhi@mail.utoronto.ca>
2019-03-17 01:59:24 -0400

    New views

--- TextTrade/tradePlatform/views.py
+++ TextTrade/tradePlatform/views.py
@@ -14,6 +14,20 @@ from django.shortcuts import render_to_response
 def index(request):
 	return HttpResponse("Hello World, you are at the Trade Platform index page.")
 
+#Clear all variables
+def clearAll():
+	Trading=False
+	Stage1=False
+	Stage2=False
+	Stage3=False
+
+#Status variables
+Trading=False
+Stage1=False
+Stage2=False
+Stage3=False
+
+
 @csrf_exempt
 def sms_response(request):
 	"""
@@ -40,46 +54,43 @@ def sms_response(request):
 	#			if a user is within a certain distance, send them a message of the Listing and tell them to send back 'Interested Listing_ID'.
 	#		if 'interested Listing_ID':
 	#			respond with a message saying 'contact 'the number of the person who made the listing' to negotiate a trade and pickup'
-	#		
-		
+	#			
 	user = User.objects.filter(phoneNumber=from_phone_number) 
 	#return HttpResponse(str(response))
 
-	if user:
+	if user and Stage1==False:
 		#text should look like "Interested in: 17" 
 		text_body_split = twilio_request.body.split(':')
 
-
-		while text_body_split[0].strip() != "Interested in" and text_body_split[0].strip() != 'Trading':
-			# NoteToSelf: Does this send a message or does it need to return to send a message?
-			
-			# If the user wants to stop the text conversation
-			if twilio_request.body.strip() == 'stop':
-				response.message("Stopped")
-				return HttpResponse(response)
-			response.message("Please send a message in the form of 'Interested in: Listing number'. for example 'Interested in: 1'. Or 'Trading' Or type 'Stop'.")
-			HttpResponse(response)
-		
-		# User has succesfully responded
-		if text_body_split[0].strip() == 'Interested in':
-			listing_id = twilio_request.body.split(':')[1].strip()
-			response.message("Your contact info has been sent to the owner of this listing")
-			return HttpResponse(response)
-			#text the owner of the listing
-			# ToDo: how to text someone else. (use avkash's code)
-			
-		elif text_body_split[0].strip() == 'Trading':
+		if text_body_split[0].strip().lower()=="trading":
+			Trading=True
+			Stage1=True
 			response.message("Please type the item you are trading followed by a ':' then the description of the item. ex: 'carrots: carrots picked 10 days ago'")
 			return HttpResponse(response)
 
+		elif text_body_split[0].strip().lower()=="interested in":
+			Stage1=True
+			listing_id = twilio_request.body.split(':')[1].strip()
+			response.message("Your contact info has been sent to the owner of this listing")
+			return HttpResponse(response)
+
+		elif text_body_split[0].strip().lower()=="stop":
+			clearAll()
+			response.message("Thank you for using TMart. Please text to begin again")
+			return HttpResponse(response)
+
+		else:
+			response.message("Please send a message in the form of 'Interested in: Listing number'. for example 'Interested in: 1'. Or 'Trading' Or type 'Stop'.")
+			return HttpResponse(response)
+
+	#elif user and Stage1==True and Stage2==False
+
 	else:
-		response.message("what is you location, type your town or address (as if into google maps)")
+		response.message("Welcome to TMart. What is you location? Please type your town or address (as if into google maps)")
 		return HttpResponse(response)
+
 
 
 
 	# Add a text message
 	#msg = response.message(str(phone_number))
-
-	return HttpResponse(str(response))
-
